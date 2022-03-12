23c6d2bb1235ea28200ad13d2d160ade7ab150c2
Gowtham K <gowthamk91@outlook.com>
2022-03-12 23:14:45 +0530

    added test methods

---
+++ HotelBooking.Application.UnitTests/Hotels/Queries/GetHotelsListQueryHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using HotelBooking.Application.Contract.Persistance;
+using HotelBooking.Application.Feature.Hotels.Queries.GetHotelsList;
+using HotelBooking.Application.Profiles;
+using HotelBooking.Application.UnitTests.Mocks;
+using HotelBooking.Domain.Entities;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HotelBooking.Application.UnitTests.Hotels.Queries
+{
+    public class GetHotelsListQueryHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IAsyncRespository<Hotel>> _mockHotelRepository;
+
+        public GetHotelsListQueryHandlerTests()
+        {
+            _mockHotelRepository = RespositoryMocks.GetHotelRepository();
+            var configurationProvider = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+            _mapper = configurationProvider.CreateMapper();
+
+        }
+        [Fact]
+        public async Task GetHotelListTest()
+        {
+            var handler = new GetHotelsListQueryHandler(_mapper, _mockHotelRepository.Object);
+            var result = await handler.Handle(new GetHotelsListQuery(), CancellationToken.None);
+            result.Count.ShouldBe(1);
+        }
+    }
+}
---
+++ HotelBooking.Application.UnitTests/Mocks/RespositoryMocks.cs
@@ -0,0 +1,25 @@
+﻿using HotelBooking.Application.Contract.Persistance;
+using HotelBooking.Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Application.UnitTests.Mocks
+{
+    public class RespositoryMocks
+    {
+        public static Mock<IAsyncRespository<Hotel>> GetHotelRepository()
+        {
+            var concertGuid = Guid.Parse("{12345678-ABCD-12AB-98AD-ABCDF1234512}");
+            var hotels = new List<Hotel>{
+                new Hotel{HotelId = concertGuid, Name="Gowtham"}
+            };
+            var mockHotelRepository = new Mock<IAsyncRespository<Hotel>>();
+            mockHotelRepository.Setup(repo => repo.ListAllAsync()).ReturnsAsync(hotels);            
+            return mockHotelRepository;
+        }
+    }
+}
